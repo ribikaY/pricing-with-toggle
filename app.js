cba3a7e118f3f73bd717bc8f1d5f19d06bb2d413
ribikaY <yonasribika@gmail.com>
2023-11-08 12:22:06 +0300

    Added vercel.json file to fix route issue

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-// document.addEventListener("DOMContentLoaded", function(){
-//     const toggleCheckbox = document.getElementById("toggleCheckbox");
+document.addEventListener("DOMContentLoaded", function(){
+    const toggleCheckbox = document.getElementById("toggleCheckbox");
 
-//     const basicPrice = document.getElementById("basic-price");
-//     const profPrice = document.getElementById("prof-price");
-//     const masterPrice = document.getElementById("master-price");
+    const basicPrice = document.getElementById("basic-price");
+    const profPrice = document.getElementById("prof-price");
+    const masterPrice = document.getElementById("master-price");
 
-//     toggleCheckbox.addEventListener("change", function(){
-//         if(toggleCheckbox.checked){
-//             basicPrice.textContent = "19.99";
-//             profPrice.textContent = "24.99";
-//             masterPrice.textContent = "39.99";
-//         }
-//         else{
-//             basicPrice.textContent = "199.99";
-//             profPrice.textContent = "249.99";
-//             masterPrice.textContent = "399.99";
-//         }
-//     })
-// })
+    toggleCheckbox.addEventListener("change", function(){
+        if(toggleCheckbox.checked){
+            basicPrice.textContent = "19.99";
+            profPrice.textContent = "24.99";
+            masterPrice.textContent = "39.99";
+        }
+        else{
+            basicPrice.textContent = "199.99";
+            profPrice.textContent = "249.99";
+            masterPrice.textContent = "399.99";
+        }
+    })
+})
